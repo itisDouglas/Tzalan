c684e3d2f3493385380c13e47904b231f37e9d8a
itisDouglas <dougcueva@gmail.com>
2021-11-23 12:52:40 -0500

    added assert macros

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::fs;
+//use std::fs::read;
+
+//use askama::Template;
+
+//template structure is here
 
 
 fn main() {
-    println!("Here's your project!");
+    println!("Your project's been generated!");
     create_project(); 
 }
 
@@ -11,28 +16,42 @@ fn main() {
 fn create_project() {
 
     let folder_path = "./Hello";
+    assert!(fs::create_dir(folder_path).is_ok());
 
     create_md();
     create_template();
+
+    // let hello = HomeTemplate {name: read("Hello/template/home.html") };
+    // println!("{}", hello.render().unwrap());
 }
 
 //this function creates the markdown folder and file
 fn create_md() {
-    let folder_path = "Hello/markdown";
+    let folder_path = "Hello/markdown/";
     let file_path = "Hello/markdown/home.md";
 
-    fs::create_dir(folder_path);
-    fs::write(file_path, b"# Hello, World!");
+    assert!(fs::create_dir(folder_path).is_ok());
+    assert!(fs::write(file_path, b"# Hello, World!").is_ok());
   
 
 }
 
-//This function creates the template fold and an html file
+//This function creates the template folder and an html file
 fn create_template() {
+
 
     let folder_path = "Hello/template";
     let file_path = "Hello/template/home.html";
+    let file_content = "Hello, {{name}}!"; 
 
-    fs::create_dir(folder_path, );
-    fs::write(file_path);
+    assert!(fs::create_dir(folder_path).is_ok());
+    assert!(fs::write(file_path, file_content).is_ok());
 }
+
+// #[derive(Template)]
+// #[template(path = "Hello/template/home.html")]
+
+// struct HomeTemplate<'a> {
+
+//     name: &'a str,
+// }
